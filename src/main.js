9b681dceafe4c96b94c035d98a902cf1dc67e5f2
zbb <2731882887@qq.com>
2020-03-14 17:20:05 +0800

    登录功能-组件的封装

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入通用的样式
+import './styles/base.less'
+//导入字体图标
+import './styles/iconfont.less'
+import 'lib-flexible'
+
+Vue.config.productionTip = false
+import router from './router'
+
+//----------------------全局注册--------------------
+
+import HmHeader from './components/HmHeader.vue'
+import HmLogo from './components/HmLogo'
+import HmButton   from './components/HmButton'
+
+
+
+Vue.component('hm-header',HmHeader)
+Vue.component('hm-logo',HmLogo)
+Vue.component('hm-button',HmButton)
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
